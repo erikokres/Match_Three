b2800e78e5efa0dfb0593917644deb67a65fc3fc
erikokres <56141967+erikokres@users.noreply.github.com>
2020-05-06 20:52:50 +0700

    add empty project

---
+++ Match Three/Assets/Script/Grid.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    public int gridSizeX, gridSizeY;
+    public Vector2 startPos, offset;
+    
+    public GameObject tilePrefab; 
+
+    public GameObject[,] tiles;
+    public GameObject[] candies;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateGrid();
+    }
+
+    // Update is called once per frame
+    void CreateGrid()
+    {
+        tiles = new GameObject[gridSizeX, gridSizeY];
+
+        Vector2 offset = tilePrefab.GetComponent<SpriteRenderer>().bounds.size;
+
+        Vector2 startPosition = transform.position +(Vector3.left * (offset.x * gridSizeX/2)) + (Vector3.down * (offset.y * gridSizeY/3));
+
+        for (int x = 0; x< gridSizeX; x++){
+            for (int y = 0; y<gridSizeY; y++){
+                Vector2 pos = new Vector3(startPosition.x + (x*offset.x), startPosition.y+(y * offset.y));
+                GameObject backgroundTile = Instantiate (tilePrefab, pos, tilePrefab.transform.rotation);
+                backgroundTile.transform.parent = transform;
+                backgroundTile.name = "(" + x + "," + y + ")";
+
+                int index = Random.Range(0, candies.Length);
+
+                GameObject candy = Instantiate(candies[index], pos, Quaternion.identity);
+                candy.name = "(" + x + "," + y + ")";
+                tiles[x, y] = candy;
+            }
+        }
+
+//         int MAX_ITERATION = 0;
+// while (MatchesAt(x, y, candies[index]) && MAX_ITERATION < 100){
+//            index = Random.Range(0, candies.Length);
+//            MAX_ITERATION++;
+// }
+// MAX_ITERATION = 0;
+    }
+private bool MatchesAt(int column, int row, GameObject piece){
+        //Cek jika ada tile yang sama dengan dibawah dan samping nya
+        if (column > 1 && row > 1){
+            if (tiles[column - 1, row].tag == piece.tag && tiles[column - 2, row].tag == piece.tag){
+                return true;
+            }
+            if (tiles[column, row - 1].tag == piece.tag && tiles[column, row - 2].tag == piece.tag){
+                return true;
+            }
+        }
+        else if (column <= 1 || row <= 1){
+            //Cek jika ada tile yang sama dengan atas dan sampingnya
+            if (row > 1){
+                if (tiles[column, row - 1].tag == piece.tag && tiles[column, row - 2].tag == piece.tag){
+                    return true;
+                }
+            }
+            if (column > 1){
+                if (tiles[column - 1, row].tag == piece.tag && tiles[column - 2, row].tag == piece.tag){
+                    return true;
+                }
+            }
+        }
+        return false;
+ }
+
+}
---
+++ Match Three/Assets/Script/Tile.cs
@@ -0,0 +1,131 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Tile : MonoBehaviour
+{
+    private Vector3 firstPosition;
+    private Vector3 finalPosition;
+    private float swipeAngle;
+    private Vector3 tempPosition;
+    //Menampung data posisi tile
+    public float xPosition;
+    public float yPosition;
+    public int column;
+    public int row;
+    private Grid grid;
+    private GameObject otherTile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Menentukan posisi dari tile
+        grid = FindObjectOfType<Grid>();
+        xPosition = transform.position.x;
+        yPosition = transform.position.y;
+        column = Mathf.RoundToInt((xPosition - grid.startPos.x) / grid.offset.x);
+        row = Mathf.RoundToInt((yPosition - grid.startPos.y) / grid.offset.x);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        xPosition = (column * grid.offset.x) + grid.startPos.x;
+        yPosition = (row * grid.offset.y) + grid.startPos.y;
+        SwipeTile();
+    }
+    void OnMouseDown()
+    {
+      //Mendapatkan titik awal sentuhan jari  
+      firstPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+    void OnMouseUp()
+    {
+        //Mendapatkan titik akhir sentuhan jari
+        finalPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        CalculateAngle();
+    }
+    void CalculateAngle()
+    {
+        //Menghitung sudut antara posisi awal dan posisi akhir
+        swipeAngle = Mathf.Atan2(finalPosition.y - firstPosition.y, finalPosition.x - firstPosition.x) * 180 / Mathf.PI;
+        MoveTile();
+    }
+    void SwipeTile()
+    {
+        if (Mathf.Abs(xPosition - transform.position.x) > .1)
+        {
+            //Move towards the target
+            tempPosition = new Vector2(xPosition, transform.position.y);
+            transform.position = Vector2.Lerp(transform.position, tempPosition, .4f);
+        }
+        else
+        {
+            //Directly set the position
+            tempPosition = new Vector2(xPosition, transform.position.y);
+            transform.position = tempPosition;
+            grid.tiles[column, row] = this.gameObject;
+        }
+        if (Mathf.Abs(yPosition - transform.position.y) > .1)
+        {
+            //Move towards the target
+            tempPosition = new Vector2(transform.position.x, yPosition);
+            transform.position = Vector2.Lerp(transform.position, tempPosition, .4f);
+        }
+        else
+        {
+            //Directly set the position
+            tempPosition = new Vector2(transform.position.x, yPosition);
+            transform.position = tempPosition;
+            grid.tiles[column, row] = this.gameObject;
+        }
+    }
+    void MoveTile()
+    {
+        if (swipeAngle > -45 && swipeAngle <= 45)
+        {
+            //Right swipe
+            SwipeRightMove();
+        }
+        else if (swipeAngle > 45 && swipeAngle <= 135)
+        {
+            //Up swipe
+            SwipeUpMove();
+        }
+        else if (swipeAngle > 135 || swipeAngle <= -135)
+        {
+            //Left swipe
+            SwipeLeftMove();
+        }
+        else if (swipeAngle < -45 && swipeAngle >= -135)
+        {
+            //Down swipe
+            SwipeDownMove();
+        }
+    }
+    void SwipeRightMove()
+    {
+        //Menukar posisi dengan sebelah kanan nya
+        otherTile = grid.tiles[column + 1, row];
+        otherTile.GetComponent<Tile>().column -= 1;
+        column += 1;
+    }
+    void SwipeUpMove()
+    {
+        //Menukar posisi dengan sebelah atasnya
+        otherTile = grid.tiles[column, row + 1];
+        otherTile.GetComponent<Tile>().row -= 1;
+        row += 1;
+    }
+    void SwipeLeftMove()
+    {
+        //Menukar posisi dengan sebelah kirinya
+        otherTile = grid.tiles[column - 1, row];
+        otherTile.GetComponent<Tile>().column += 1;
+        column -= 1;
+    }
+    void SwipeDownMove()
+    {
+        //Menukar posisi denhgan sebelah bawahnya
+        otherTile = grid.tiles[column, row - 1];
+        otherTile.GetComponent<Tile>().row += 1;
+        row -= 1;
+    }
+}
